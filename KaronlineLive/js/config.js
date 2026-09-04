862dcfa9fd15423779bff0cea46b704b4ec22060
KARONLINE64 <karonline64@users.noreply.github.com>
2026-09-04 18:28:06 +0200

    Use immutable V90.6 setup download

--- KaronlineLive/js/config.js
+++ KaronlineLive/js/config.js
@@ -67,5 +67,5 @@ function getCatalogueUrl() {
 }
 
 function getKaronlineBoxDownloadUrl() {
-  return 'downloads/KaronlineBox_V90_6_Setup.exe';
+  return 'https://raw.githubusercontent.com/KARONLINE64/karonline-live-private/karonlinelive-site/downloads/KaronlineBox_V90_6_Setup.exe';
 }
